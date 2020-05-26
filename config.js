01887f62c5d600f9d44b78680d7243febd4d5f62
wm <wm@wiesmac.local>
2020-05-26 10:20:44 +0200

    gitignore

--- config.js
+++ config.js
@@ -18,18 +18,18 @@
 angular.module('main')
   .constant('st2Config', {
 
-    // hosts: [
-    //   {
-    //     name: 'Dev Env',
-    //     url: 'https://:443/api',
-    //     auth: 'https://:443/auth',
-    //     stream: 'https://:443/stream',
-    //   },
-    //   {
-    //     name: 'Express',
-    //     url: '//172.168.90.50:9101/api',
-    //     auth: '//172.168.90.50:9101/auth',
-    //   },
-    // ],
+    hosts: [
+      {
+        name: 'Dev Env',
+        url: 'https://10.10.10.10:443/api',
+        auth: 'https://10.10.10.10:443/auth',
+        stream: 'https://10.10.10.10:443/stream',
+      },
+      // {
+      //   name: 'Express',
+      //   url: '//172.168.90.50:9101/api',
+      //   auth: '//172.168.90.50:9101/auth',
+      // },
+    ],
 
   });
